78c212b339991101378db496e0ca988fbfcdfabc
ruoN23 <aboubakarinourislam@gmail.com>
2026-08-24 12:19:31 +0000

    auth-api with keycloak - docker-compose incluiding the keycloal part

--- mysql/init/01-create-databases.sql
+++ mysql/init/01-create-databases.sql
@@ -1,5 +1,5 @@
 -- Exécuté automatiquement au tout premier démarrage du conteneur mysql
  
-CREATE DATABASE IF NOT EXISTS auth_db;
+CREATE DATABASE IF NOT EXISTS keycloak_db;
 CREATE DATABASE IF NOT EXISTS employee_db;
  
